cda8a8bec4e5b239747a1749b78618cec7283144
Leo <medinayleo@gmail.com>
2020-10-09 13:01:09 -0400

    lol iI can't believe I got the form to work on the first try

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../init_data.json').data;
+var _ = require('lodash');
 
 router.get('/', function (req, res) {
   res.render('home');
@@ -7,6 +9,11 @@ router.get('/', function (req, res) {
 
 router.get('/signup', function (req, res) {
   res.render('signup');
+});
+
+router.get('/admin', function (req, res) {
+  const usersArray = _.toArray(users);
+  res.render('admin', {users: usersArray});
 });
 
 module.exports = router;
